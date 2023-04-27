bc40603b070ae61eba769bed976840f8028b6755
q9good <psnyima@gmail.com>
2023-04-27 22:25:54 +0800

    strange

--- plugins/installs/init.lua
+++ plugins/installs/init.lua
@@ -10,14 +10,34 @@ return {
         end
     },
     { "echasnovski/mini.align", version = false },
-    { "simrat39/rust-tools.nvim", lazy = false,
-        config = function ()
-            require("rust-tools").setup(
-                require("custom.plugins.installs.rust-tools")
-            )
+--     { "simrat39/rust-tools.nvim", lazy = false,
+--         config = function ()
+--             require("rust-tools").setup(
+--                 require("custom.plugins.installs.rust-tools")
+--             )
+--         end
+--     },
+    {
+        "rust-lang/rust.vim",
+        ft = "rust",
+        init = function()
+            vim.g.rustfmt_autosave = 1
         end
     },
---   {
+    {
+        'saecki/crates.nvim',
+        ft = {"rust", "toml"},
+        config = function(_, opts)
+            local crates = require('crates')
+            crates.setup(opts)
+            require('cmp').setup.buffer({
+                sources = {{name = crates}}
+            })
+            crates.show()
+            require("core.utils").load_mappings("crates")
+        end,
+    },
+-- --   {
 --     "max397574/better-escape.nvim",
 --     event = "InsertEnter",
 --     config = function()
